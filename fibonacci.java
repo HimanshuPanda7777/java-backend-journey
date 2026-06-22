b99e184775f54b013bff083cf7b92f866b6026c9
Himanshu Panda <panda.himanshu7777@gmail.com>
2026-06-22 20:54:06 +0530

    Fibonacci Series

---
+++ fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class fibonacci{
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n = sc.nextInt();
+    int a=0;
+    int b=1;
+    System.out.print("Fibonacci series:");
+    for(int i=0;i<n;i++){
+        
+        System.out.print(a+" ");
+        int next =a+b;
+        a=b;
+        b=next;
+    }
+}
+}
